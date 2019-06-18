22881c1d8a8509a123e6b3e9a626e2c909f1363b
Niki Pourazin <nikipourzin@gmail.com>
2019-06-18 14:09:28 +0430

    Cool graphics added.

--- src/GUI/MyLayerUI.java
+++ src/GUI/MyLayerUI.java
@@ -2,11 +2,31 @@ package GUI;
 
 import javax.swing.*;
 import javax.swing.plaf.LayerUI;
+import java.awt.*;
 
-public class MyLayerUI extends LayerUI<JPanel> {
-    public MyLayerUI(){
+public class MyLayerUI extends LayerUI<Component> {
+    MyLayerUI() {
         super();
-
-        //we still dont know
     }
+
+    @Override
+    public void paint(Graphics g, JComponent c) {
+        super.paint(g, c);
+
+        Graphics2D g2 = (Graphics2D) g.create();
+
+        int w = c.getWidth();
+        int h = c.getHeight();
+        g2.setComposite(AlphaComposite.getInstance(
+                AlphaComposite.SRC_OVER, .5f));
+        g2.setPaint(new GradientPaint(0, 0, Color.yellow, 0, h, Color.red));
+        g2.fillRect(0, 0, w, h);
+
+        g2.dispose();
+    }
+
+
+    ///
+
+
 }
