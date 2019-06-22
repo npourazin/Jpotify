2549e0d54657c7e0512879160953b10a91f1694e
Niki Pourazin <nikipourzin@gmail.com>
2019-06-22 15:30:16 +0430

    Scroller for choices panel made.

--- src/Controllers/ClickListenerForShowingSongsList.java
+++ src/Controllers/ClickListenerForShowingSongsList.java
@@ -1,4 +1,5 @@
 package Controllers;
+import GUI.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,5 +8,8 @@ public class ClickListenerForShowingSongsList implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         //TODO show that damned songs here and give each of them a button.
+        ChoicesArea choicesArea = (ChoicesArea)actionEvent.getSource();
+        choicesArea.validate();
+
     }
 }
--- src/GUI/ChoicesArea.java
+++ src/GUI/ChoicesArea.java
@@ -7,7 +7,7 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 
 
-class ChoicesArea extends JPanel{
+public class ChoicesArea extends JPanel{
     private JScrollPane jScrollPane ;
 
     ChoicesArea(){
@@ -15,12 +15,15 @@ class ChoicesArea extends JPanel{
 
 //        jScrollPane = new JScrollPane();
 //        this.add(jScrollPane, BorderLayout.WEST);
-        JScrollPane jScrollPane = new JScrollPane(this,   ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+         jScrollPane = new JScrollPane(this);
+//         jScrollPane = new JScrollPane(this,   ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 
 
-        jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        jScrollPane.setViewportBorder(new LineBorder(Color.RED));
+        jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        jScrollPane.setViewportBorder(new LineBorder(Color.pink));
+        jScrollPane.setBackground(Color.cyan);
+        jScrollPane.updateUI();
         jScrollPane.setVisible(true);
 
 
@@ -36,7 +39,7 @@ class ChoicesArea extends JPanel{
         songsButton.setFont(songsButton.getFont().deriveFont(14f)); // will only change size to 14pt
 
         this.add(songsButton);
-        songsButton.addActionListener(new ClickListenerForAddingSongs());
+        songsButton.addActionListener(new ClickListenerForShowingSongsList());
         try {
             Image img = ImageIO.read(getClass().getResource("songs1.png"));
             songsButton.setIcon(new ImageIcon(img));
@@ -70,6 +73,23 @@ class ChoicesArea extends JPanel{
         JList list = new JList(model);
 //        jScrollPane.add(list);
 
+    }
+    @Override
+    public void paint(Graphics g) {
+
+        super.paint(g);
+
+        Graphics2D g2 = (Graphics2D) g.create();
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+        //this line
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .5f));
+
+        g2.setPaint(new GradientPaint(0, 0, Color.pink, 0, h, Color.cyan));
+        g2.fillRect(0, 0, w, h);
+
+        g2.dispose();
     }
 
     public JScrollPane getjScrollPane() {
