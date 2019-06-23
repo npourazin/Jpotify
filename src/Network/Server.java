3d147e63808bea4870f1405d221602c5ea29130e
Mahvash Siavashpour <mahvash-siavashpour@users.noreply.github.com>
2019-06-23 16:31:43 +0430

    Network package added. Server class added.

---
+++ src/Network/Server.java
@@ -0,0 +1,65 @@
+package Network;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server implements Runnable {
+    private ServerSocket serverSocket;
+    ExecutorService executorService;
+    private boolean isRun;
+
+    public Server() throws IOException {
+        this.serverSocket = new ServerSocket(2000);
+        this.executorService = Executors.newCachedThreadPool();
+        isRun = true;
+    }
+
+    @Override
+    public void run() {
+        while (isRun) {
+            try {
+                Socket client = this.serverSocket.accept();
+                this.executorService.submit(new Handler(client));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String sendData() {
+        //TODO: send song name
+        return "Example: Dean Lewis";
+    }
+
+    private static class Handler implements Runnable {
+
+        private Socket client;
+
+        public Handler(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                int result;
+                BufferedReader bfRead = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String string = bfRead.readLine();
+                System.out.println("Message from client " + client.getInetAddress() + ":" + string);
+
+
+                PrintWriter bfWriter = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+                bfWriter.println(sendData());
+                bfWriter.flush();
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
