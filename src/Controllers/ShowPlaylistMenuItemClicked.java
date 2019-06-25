007585665f90b4ef631e91c0b897b16decf9699b
Mahvash Siavashpour <mahvash-siavashpour@users.noreply.github.com>
2019-06-25 19:54:08 +0430

    showing songs from playlist feature added.

--- src/Controllers/ShowPlaylistMenuItemClicked.java
+++ src/Controllers/ShowPlaylistMenuItemClicked.java
@@ -1,8 +1,12 @@
 package Controllers;
 
+import GUI.PlaylistPanel;
+import GUI.SongsPanel;
 import Logic.Main;
+import Logic.PlayerManager;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,5 +17,21 @@ public class ShowPlaylistMenuItemClicked implements ActionListener {
         System.out.println(SelectedPlaylistListener.getPlaylistName());
         System.out.println("clicked");
         Main.creatCurrentQueue("src/"+SelectedPlaylistListener.getPlaylistName()+".txt");
+
+
+        //cleaning everything
+        Main.getJpotifyGUI().getHomePanel().setVisible(true);
+        Main.getJpotifyGUI().getHomePanel().removeAll();
+        Main.getJpotifyGUI().getContentPane().invalidate();
+        Main.getJpotifyGUI().getContentPane().revalidate();
+        Main.getJpotifyGUI().getContentPane().repaint();
+
+
+        //making new panel show up
+        PlaylistPanel playlistPanel = new PlaylistPanel(Main.getCurrentQueue());
+        Main.getJpotifyGUI().setPlaylistPanel(playlistPanel);
+        Main.getJpotifyGUI().getHomePanel().removeAll();
+        Main.getJpotifyGUI().getHomePanel().add(Main.getJpotifyGUI().getPlaylistPanel(), BorderLayout.CENTER);
+        Main.getJpotifyGUI().revalidate();
     }
 }
