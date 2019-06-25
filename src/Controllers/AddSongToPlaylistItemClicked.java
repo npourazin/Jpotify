083c47fd541a357a405fa31e43e574f87b8e56ab
Mahvash Siavashpour <mahvash-siavashpour@users.noreply.github.com>
2019-06-25 21:42:20 +0430

    Remove songs from playlist feature added.

--- src/Controllers/AddSongToPlaylistItemClicked.java
+++ src/Controllers/AddSongToPlaylistItemClicked.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import GUI.DialogBoxList;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,43 +14,45 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class AddSongToPlaylistItemClicked implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            PrintWriter playlistFR = new PrintWriter(new FileWriter(new File("src/" + SelectedPlaylistListener.getPlaylistName() + ".txt"), true));
+
+        DialogBoxList aggDialog=new DialogBoxList("AddedSongAdresses",new SelectSongToAddToPlaylistListener());
+
+//        try {
 //            JFrame jFrame = new JFrame("All Songs");
 //            jFrame.setVisible(true);
 //            jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            JDialog addDialog=new JDialog();
-            addDialog.setLayout(new GridLayout(1,2));
-            addDialog.setSize(200, 100);
-            addDialog.setBackground(Color.pink);
-            addDialog.setVisible(true);
-            addDialog.getDefaultCloseOperation();
-            DefaultListModel model = new DefaultListModel();
-            JList list = new JList(model);
-            list.setVisible(true);
-            list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-            list.setLayoutOrientation(JList.VERTICAL);
-            list.setVisibleRowCount(-1);
-            list.addListSelectionListener(new SelectSongToAddToPlaylistListener());
-            addDialog.add(list);
-            try {
+//            JDialog addDialog = new JDialog();
+//            addDialog.setLayout(new GridLayout(1, 2));
+//            addDialog.setSize(200, 100);
+//            addDialog.setBackground(Color.pink);
+//            addDialog.setVisible(true);
+//            addDialog.getDefaultCloseOperation();
+//            DefaultListModel model = new DefaultListModel();
+//            JList list = new JList(model);
+//            list.setVisible(true);
+//            list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//            list.setLayoutOrientation(JList.VERTICAL);
+//            list.setVisibleRowCount(-1);
+//            list.addListSelectionListener(new SelectSongToAddToPlaylistListener());
+//            addDialog.add(list);
+//            try {
+//
+//                //Showing the list of the songs paths
+//                String playlistName;
+//                Scanner sc = new Scanner(new FileReader(new File("src/AddedSongAdresses.txt")));
+//                while (sc.hasNext()) {
+//                    playlistName = sc.nextLine();
+//                    model.addElement(playlistName);
+//                }
+//
+//            } catch (FileNotFoundException ex) {
+//                ex.printStackTrace();
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//        } finally {
+//
+//        }
 
-                //Todo why new file?
-                //Showing the list of the songs paths
-                String playlistName;
-                Scanner sc = new Scanner(new FileReader(new File("src/AddedSongAdresses.txt")));
-                while (sc.hasNext()) {
-                    playlistName = sc.nextLine();
-                    model.addElement(playlistName);
-                }
-
-            } catch (FileNotFoundException ex) {
-                ex.printStackTrace();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
     }
 }
--- src/Controllers/RemoveSongFromPlaylistItemClicked.java
+++ src/Controllers/RemoveSongFromPlaylistItemClicked.java
@@ -1,11 +1,21 @@
 package Controllers;
 
+import GUI.DialogBoxList;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class RemoveSongFromPlaylistItemClicked implements ActionListener {
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        DialogBoxList removeDialog=new DialogBoxList(SelectedPlaylistListener.getPlaylistName(),new SelectSongToRemoveFromPlaylist());
     }
 }
