a3baccb2e18e2054e1767eefae82a4df73b10bcb
Mahvash Siavashpour <mahvash-siavashpour@users.noreply.github.com>
2019-06-27 18:52:27 +0430

    Sorting albums by time.

--- src/GUI/AlbumsPanel.java
+++ src/GUI/AlbumsPanel.java
@@ -2,13 +2,15 @@ package GUI;
 
 import Controllers.ShowSpecificAlbumOnClick;
 import Logic.Music;
+import Logic.PlayerManager;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.Scanner;
 
 public class AlbumsPanel extends JPanel {
@@ -22,7 +24,7 @@ public class AlbumsPanel extends JPanel {
         this.setLayout(new FlowLayout());
         this.setVisible(true);
         albumButton = new ArrayList<>();
-        albums=new ArrayList<>();
+        albums = new ArrayList<>();
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -37,6 +39,10 @@ public class AlbumsPanel extends JPanel {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        //sort albim by time
+        sortAlbums();;
+
 
         Dimension d = new Dimension(180, 180);
         for (int i = 0; i < albums.size(); i++) {
@@ -55,22 +61,21 @@ public class AlbumsPanel extends JPanel {
             }
             this.repaint();
             this.revalidate();
-            Scanner sc= null;
+            Scanner sc = null;
             try {
-                sc = new Scanner(new FileReader(new File("src/"+albums.get(i)+".txt")));
+                sc = new Scanner(new FileReader(new File("src/" + albums.get(i) + ".txt")));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-            boolean flag=false;
-            Music music=null;
-            if (sc.hasNext()){
-                music=new Music(sc.nextLine());
-                flag=true;
+            boolean flag = false;
+            Music music = null;
+            if (sc.hasNext()) {
+                music = new Music(sc.nextLine());
+                flag = true;
             }
-            if(flag){
+            if (flag) {
                 albumButton.get(i).setIcon(new ImageIcon(((ImageIcon) music.getSongData().getIcon()).getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
-            }
-            else {
+            } else {
                 try {
 
                     Image img = ImageIO.read(getClass().getResource("defaultSongIcon.png"));
@@ -80,12 +85,35 @@ public class AlbumsPanel extends JPanel {
                 }
             }
 
-            JTextArea a = new JTextArea("Album:"+albums.get(i));
+            JTextArea a = new JTextArea("Album:" + albums.get(i));
             albumButton.get(i).add(a, BorderLayout.SOUTH);
 
 
         }
     }
+
+    //sort albim by time
+    public void sortAlbums() {
+        ArrayList<Date> temp=new ArrayList<>();
+        for (int i = 0; i <albums.size() ; i++) {
+            for (int j = 0; j < PlayerManager.getSongDataArrayList().size(); j++) {
+                if(albums.get(i).equals(PlayerManager.getSongDataArrayList().get(j).getAlbum())){
+                    temp.add(PlayerManager.getSongDataArrayList().get(j).getLastTimeListened());
+                }
+            }
+        }
+
+        for (int i = 0; i <temp.size()-1 ; i++) {
+            for (int j = 0; j <temp.size()-1-i ; j++) {
+                if(temp.get(j).before(temp.get(j+1))){
+                    Collections.swap(temp,j,j+1);
+                    Collections.swap(albums,j,j+1);
+                }
+            }
+        }
+
+    }
+
 
     public JScrollPane getjScrollPane() {
         return jScrollPane;
