06eddd25639b728f578c56cd43f7b30f36f3a091
Niki Pourazin <nikipourzin@gmail.com>
2019-06-29 21:59:16 +0430

    Minor editing.

--- src/Controllers/JpotifyWindowActionListener.java
+++ src/Controllers/JpotifyWindowActionListener.java
@@ -9,6 +9,7 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.*;
 
+
 public class JpotifyWindowActionListener implements WindowListener {
     @Override
     public void windowOpened(WindowEvent windowEvent) {
